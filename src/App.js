93867c98ea79dcc31bc45460b993ffcbe6565153
LiuJQ9674Git <455298138@qq.com>
2016-05-31 06:08:18 +0800

    reactboot config

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
 import React from 'react';
 import 'bootstrap/less/bootstrap.less';
+import './styles/custom-styles.css';
 
-React.render(<h1>Hello Bootrappers</h1>, document.getElementById('app'));
+import HeaderNavigation from './HeaderNavigation';
+import Body from './Body';
+import Footer from './Footer';
+
+global.React = React;
+
+React.render(
+    <div>
+        <HeaderNavigation />
+        <Body />
+        <Footer />
+    </div>
+    , document.getElementById('app'));
